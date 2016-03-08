70981f76daa998c082ad4d2b08a545d57886abce
Joe Perry <joseph.w.perry@gmail.com>
2016-03-07 19:25:49 -0700

    This week implemented, dashboard styling finished

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -1,5 +1,6 @@
 class DashboardPresenter < SimpleDelegator
   attr_reader :view, :model
+  require 'date'
 
   def initialize(model, view)
     @view = view
@@ -15,5 +16,12 @@ class DashboardPresenter < SimpleDelegator
     time = (((event.time + event.utc_offset).to_f)/1000).to_s
     date = DateTime.strptime((time), '%s')
     date.strftime("%m/%d/%Y %l:%M %P")
+  end
+
+  def current_events
+    current_time = DateTime.now.strftime('%Q').to_i
+    @model.events.select do |event|
+      ((event.time + event.utc_offset) - (current_time + event.utc_offset)) <= 604800000
+    end.reverse
   end
 end
