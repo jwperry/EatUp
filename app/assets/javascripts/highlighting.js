6d2688516bee313aef7a070d147f79c3cfea7734
Joe Perry <joseph.w.perry@gmail.com>
2016-03-10 08:07:32 -0700

    HighLIGHTNING

--- app/assets/javascripts/highlighting.js
+++ app/assets/javascripts/highlighting.js
@@ -25,3 +25,4 @@ $(function(){
     $(this).html(search_target);
   });
 });
+
--- public/assets/application-02c5ffcad3b834d06c0f9f4286e9108de1c77a1983fd1bcf37ef400d1aca287b.js
+++ public/assets/application-02c5ffcad3b834d06c0f9f4286e9108de1c77a1983fd1bcf37ef400d1aca287b.js
@@ -11560,28 +11560,33 @@ return jQuery;
   }
 
 })( jQuery );
-// var highlight_text = function(search_term, search_target) {
-//   var new_string = search_target.replace(new RegExp(search_term, "ig"), "<span style='background-color: yellow'>$&</span>");
-//   return new_string;
-// }
+var highlight_text = function(search_term, search_target) {
+  var new_string = search_target.replace(new RegExp(search_term, "ig"), "<span style='background-color: yellow'>$&</span>");
+  return new_string;
+}
 
-// var search_terms = [ "free food", "free pizza", "free slice", "free snacks", "free sandwich",
-//                      "free sandwiches," "free refreshments", "will be served",
-//                      "eat free", "free eats", "free drinks", "free beer", "free wine",
-//                      "free cocktails", "free meal", "free homecooked", "free home-cooked",
-//                      "free home cooked", "free dessert", "provide food", "provide pizza",
-//                      "provide snacks", "provide sandwiches", "provide drinks", "provide beer",
-//                      "provide wine", "provide cocktails", "provide dessert",
-//                      "provide ice cream", "provide eats", "with pizza", "with snacks",
-//                      "with refreshments"];
+var search_terms = [ "free food", "free pizza", "free slice", "free snacks", "free sandwich", 
+                     "free sandwiches", "free refreshments", "will be served",
+                     "eat free", "free eats", "free drinks", "free beer", "free wine",
+                     "free cocktails", "free meal", "free homecooked", "free home-cooked",
+                     "free home cooked", "free dessert", "provide food", "provide pizza",
+                     "provide snacks", "provide sandwiches", "provide drinks", "provide beer",
+                     "provide wine", "provide cocktails", "provide dessert",
+                     "provide ice cream", "provide eats", "with pizza", "with snacks",
+                     "with refreshments"];
 
-// var search_target = $(".description-text").text();
 
-// for (var i = 0; i < search_terms.length; i++)
-// {
-//   search_target = highlight_text(search_terms[i], search_target);
-// }
-// $(".description-text").html(search_target);
+$(function(){
+  $(".description-text").each(function(){
+    var search_target = $(this).text();
+
+    for (var i = 0; i < search_terms.length; i++)
+    {
+      search_target = highlight_text(search_terms[i], search_target);
+    }
+    $(this).html(search_target);
+  });
+});
 $(function(){  
   $(".scheduler-events-card-wrapper").on("click", ".scheduler-event-wrapper", function(){
     $(this).hide().appendTo(".scheduler-selected-card-wrapper").fadeIn("slow");
@@ -11616,29 +11621,6 @@ $(".clear-button").click(function(event){
     
   });
 });
-
-var highlight_text = function(search_term, search_target) {
-  var new_string = search_target.replace(new RegExp(search_term, "ig"), "<span style='background-color: yellow'>$&</span>");
-  return new_string;
-}
-
-var search_terms = [ "free food", "free pizza", "free slice", "free snacks", "free sandwich",
-                     "free sandwiches," "free refreshments", "will be served",
-                     "eat free", "free eats", "free drinks", "free beer", "free wine",
-                     "free cocktails", "free meal", "free homecooked", "free home-cooked",
-                     "free home cooked", "free dessert", "provide food", "provide pizza",
-                     "provide snacks", "provide sandwiches", "provide drinks", "provide beer",
-                     "provide wine", "provide cocktails", "provide dessert",
-                     "provide ice cream", "provide eats", "with pizza", "with snacks",
-                     "with refreshments"];
-
-var search_target = $(".description-text").text();
-
-for (var i = 0; i < search_terms.length; i++)
-{
-  search_target = highlight_text(search_terms[i], search_target);
-}
-$(".description-text").html(search_target);
 /* ========================================================================
  * Bootstrap: affix.js v3.3.6
  * http://getbootstrap.com/javascript/#affix
